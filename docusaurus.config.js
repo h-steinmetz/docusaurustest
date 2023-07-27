1b02234ab68e1a4af926a18dcaa583eef07be08f
Herbert Steinmetz <h.steinmetz@cryptix.ag>
2023-07-27 09:17:06 +0200

    add tag manager

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'GTM-344932somethin',
+        },
       }),
     ],
   ],
